3bfc16105bc3891f47b9e0d4fc905937becc4734
ItzelJuarez <60714282+ItzelJuarez@users.noreply.github.com>
2020-02-12 18:11:31 -0600

    Liquid Test
    - Added measurements to liquid bottles
    - Controlled measurements
    - Slower liquid flow
    - Liquid collider
    - Added new bar scene
    - Throwable pickables
    - Better hand material
    - Post processing effects and volumes
    - Lighting optimized
    - Organized project

---
+++ VRCapstone_2.0/Assets/Scripts/CubeRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRespawn : MonoBehaviour
+{
+    public Transform spawnPt;
+    public GameObject spawnObj;
+    public GameObject particleObj;
+
+    public AudioSource aus;
+    public AudioClip myClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            aus.PlayOneShot(myClip);
+            Destroy(other.gameObject);
+            GameObject go = Instantiate(spawnObj, spawnPt.transform.position, Quaternion.identity);
+            GameObject go2 = Instantiate(particleObj, other.transform.position, Quaternion.identity);
+        //    GameObject go3 = Instantiate(particleObj, spawnPt.transform.position, Quaternion.identity);
+        }
+    }
+}
